67538ba9d678ad59c7a0f29805d31429b494c175
alexislendechy <alexislendechy@gmail.com>
2023-08-23 12:01:44 -0600

    finished

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# LogoMaker
+# Logo Maker
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+This application will allow you to create simple logo for your clients and projects so that they can forego paying a graphic designer. It utilizes inquirer to prompt the user within the command line for how they would like their logo to look like. 
+
+## Technologies Used
+
+This project is powered by Node.js v16, utilizes inquirer v8.2.4 (node package manager), and file system module (node package manager). It also employs jest v29.5.0 (node package manager) for the unit testing conducted in this application. 
+
+## Installation
+
+1. Click on the file to open it in the terminal.
+
+2. Run the command **npm i** to install the dependencies associated with this application (developers may need to install inquirer and jest directly from the command line, to do so the command for inquirer will be npm i inquirer@8.2.4 to install v8.2.4 of the inquirer, and npm i jest to install the latest version of jest).
+
+3. To run the application, within the terminal, type the command **node index.js**.
+
+
+```bash
+Please see recording below these steps are followed:
+```
+[R e c o r d i n g](https://drive.google.com/file/d/1xkf7BeX06gSZ9IreuWzmipgFcn0GFZxu/view)
+
+
+
+**NOTE** Once the application finished, the logo will be created **BUT** keep in mind that it will only be visible as an image whith the extension **Svg Preview** installed.
+
+![Screenshot1-week-10-challenge](./svg.JPG)
+
+
+
+## License
+
+NOTICE: This application is covered under the MIT License
+
+## Questions
+
+Have additional questions? Click the links below to reach me through my GitHub account or Email address.
+
+[Link to Github](https://github.com/alexislendechy)
+
+[Email](alexislendechy@gmail.com)
---
+++ index.js
@@ -0,0 +1,82 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Triangle, Square, Circle } = require("./lib/shapes");
+
+// Function writes the SVG file using user answers from inquirer prompts
+const writeToFile = (fileName, answers) => {
+  let svgString = "";
+  svgString =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  svgString += "<g>";
+  svgString += `${answers.shape}`;
+
+  // Conditional check takes users input from choices array and then adds polygon properties and shape color to SVG string
+  let shapeChoice;
+  if (answers.shape === "Triangle") {
+    shapeChoice = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+  } else if (answers.shape === "Square") {
+    shapeChoice = new Square();
+    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+  } else {
+    shapeChoice = new Circle();
+    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+  }
+
+  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+  svgString += "</g>";
+  svgString += "</svg>";
+
+  // Using file system module to generate svg file, takes in file name given in the promptUser function, the svg string, and a ternary operator which handles logging any errors, or a "Generated logo.svg" message to the console  
+  fs.writeFile(fileName, svgString, (err) => {
+    err ? console.log(err) : console.log("Generated logo.svg");
+  });
+}
+
+// This function utilizes inquirer .prompt to prompt the user to answer questions in the command line and save user input
+const promptUser = () => {
+  inquirer
+    .prompt([
+      // Text prompt
+      {
+        type: "input",
+        message:
+          "What text would you like you logo to display? (Enter up to three characters)",
+        name: "text",
+      },
+      // Text color prompt
+      {
+        type: "input",
+        message:
+          "Choose text color (Enter color keyword OR a hexadecimal number)",
+        name: "textColor",
+      },
+      // Shape choice prompt
+      {
+        type: "list",
+        message: "What shape would you like the logo to render?",
+        choices: ["Triangle", "Square", "Circle"],
+        name: "shape",
+      },
+      // Shape color prompt
+      {
+        type: "input",
+        message:
+          "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+        name: "shapeBackgroundColor",
+      },
+    ])
+    .then((answers) => {
+      // Error handling for text prompt (user must enter 3 characters or less for logo to generate)
+      if (answers.text.length > 3) {
+        console.log("Must enter a value of no more than 3 characters");
+        promptUser();
+      } else {
+        // Calling write file function to generate SVG file
+        writeToFile("logo.svg", answers);
+      }
+    });
+}
+
+// Calling promptUser function so inquirer prompts fire off when application is ran
+promptUser();
